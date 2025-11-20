c851cf8e2fbabb0c0b949c021616043ba840a704
ArthurWillers <arthurwillers@proton.me>
2025-11-19 21:53:27 -0300

    fix: update link in guest layout to point to the welcome route

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -2,7 +2,7 @@
     <div class="flex min-h-svh flex-col">
         <div class="bg-background flex flex-1 flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
-                <a href="" class="flex flex-col items-center gap-2 font-medium">
+                <a href="{{ route('welcome') }}" class="flex flex-col items-center gap-2 font-medium">
                     <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-lg">
                         <x-app-logo-icon/>
                     </span>
